d0c9fbbd7bd7e724be7a500060cf5bcbd6825215
Aaron Mathew <aaronmathew453@gmail.com>
2024-04-29 22:39:00 +0530

    Asynchronus Code error handler created

--- app.js
+++ app.js
@@ -12,8 +12,11 @@ app.use("/", require("./routes/indexRoutes"));
 
 //error handling
 const ErrorHandler = require("./utils/ErrorHandler");
+const { generatedErrors } = require("./middlewares/errors");
 app.all("*", (req,res,next) =>{
     next(new ErrorHandler(`Requested URL Not Found ${req.url}`, 404));
 });
+app.use(generatedErrors);
+
 
 app.listen(process.env.PORT, console.log(`Server running on port ${process.env.PORT}`));
---
+++ middlewares/errors.js
@@ -0,0 +1,10 @@
+exports.generatedErrors = (err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+
+    res.status(statusCode).json({
+        message: err.message,
+        errName: err.name,
+        // stack: err.stack,
+//This will handle our synchronus error {video= 0:10:42}, commit this change aaron say {Asynchronus Code error handler created}
+    });
+};
