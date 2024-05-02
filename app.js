fa9555b895748842ddaf0ba155018731ebd602dc
Aaron Mathew <aaronmathew453@gmail.com>
2024-05-02 22:46:05 +0530

    Connected to database through mongosh

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ require("dotenv").config({ path: './.env' });
 const express = require("express");
 const app = express();
 
+// db connection
+require("./models/datbase").connectDatabase();
+
 //logger
 const logger = require("morgan");
 app.use(logger("tiny"));
--- models/datbase.js
+++ models/datbase.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 exports.connectDatabase = async () => {
     try {
-        await mongoose.connect("");
+        await mongoose.connect(process.env.MONGODB_URL);
         console.log('Databse Connection Established!');
     } catch (error) {
         console.log(error.message);
     }
 }
 
-// continue video from{00:02:26}
+// continue video from{00:05:06}
